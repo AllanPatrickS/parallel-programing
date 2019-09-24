592b16f5a5527e2119bf4170202d7a35d8db25fd
Arthur Silva <arthursilva@localhost.localdomain>
2019-09-24 00:12:07 -0300

    Adiciona tempo matriz open

--- matriz_open.c
+++ matriz_open.c
@@ -1,15 +1,20 @@
 #include <omp.h>
 #include <math.h>
 #include <time.h>
-#include <iostream>
+#include <stdio.h>
 
-#define SIZE 50
+/**
+* gcc matriz_open.c -o matriz_open -fopenmp
+*/
+
+#define SIZE 100
 int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
  
 int main(int argc, char *argv[]) {
-    int i, j, k,N, nthreads;
+    int i, j, k, nthreads;
     clock_t clock_timer;
     double wall_timer;
+
 
 	// Inicializa Matriz com 1
 	for(i = 0 ; i < SIZE; i++){
@@ -20,14 +25,18 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-   #pragma omp parallel shared(A,B,C,N) private(i,j,k)
+	double begin = omp_get_wtime();
+	#pragma omp parallel shared(A,B,C) private(i,j,k)
 	#pragma omp for 
 	for(i = 0; i < SIZE; i++){
-		for( j = 0; j < N; j++) {
+		for( j = 0; j < SIZE; j++) {
 			C[i][j] = 0.0;
-			for (k = 0; k < N ; k++) {
+			for (k = 0; k < SIZE ; k++) {
 				C[i][j] = C[i][j] + A[i][k] * B[k][j];
 			}
 		}
 	}
+	double end = omp_get_wtime();
+	double time_spent = (end - begin);
+	printf("Tempo total: %f secs\n", time_spent);
 }
